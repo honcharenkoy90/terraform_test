beeb7a33075f4da2939f885ef1839fe25e7ffca7
Yurii Honcharenko <honcharenkoy90@gmail.com>
2018-08-09 16:53:48 +0300

    changes before validate

--- connect.tf
+++ connect.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_file = "C:/Users/Workstation/Documents/aws/credentials"
+  shared_credentials_file = "C:/Users/Workstation/.aws/credentials"
   profile                 = "terraform"
 }
