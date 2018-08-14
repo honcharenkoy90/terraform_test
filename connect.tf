2a7bf47daa23984a5b26209bd39e1e90de964ba5
Yurii.Honcharenko <honcharenkoy90@gmail.com>
2018-08-14 12:47:25 +0200

    Test

--- connect.tf
+++ connect.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_file = "C:/Users/Workstation/.aws/credentials"
-  profile                 = "tf_user"
+  shared_credentials_file = "/home/vagrant/.aws/creds"
+  profile                 = "tf_user2"
 }
