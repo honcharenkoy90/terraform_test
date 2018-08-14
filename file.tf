c10923b3c2d3f5a6f48336b0c852c8f4e71f03a7
yurii.honcharenko <honcharenkoy90@gmail.com>
2018-08-14 16:01:19 +0200

    Working changes in file.tf

--- file.tf
+++ file.tf
@@ -1,70 +1,85 @@
 resource "aws_security_group" "InstanceSecGroup" {
   vpc_id = "vpc-09fc024f9212e9c1f"
+
   ingress = [
     {
       from_port = 22
-      protocol = "tcp"
-      to_port = 22
+      protocol  = "tcp"
+      to_port   = 22
     },
     {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
-    }
+      from_port = 80
+      protocol  = "tcp"
+      to_port   = 80
+    },
   ]
+
   egress = [
     {
       from_port = 22
-      protocol = "tcp"
-      to_port = 22
-},
+      protocol  = "tcp"
+      to_port   = 22
+    },
     {
       from_port = 80
-      protocol = "tcp"
-      to_port = 80
-    }
+      protocol  = "tcp"
+      to_port   = 80
+    },
   ]
-
 }
 
 resource "aws_alb_target_group" "AlbTargetGroup" {
-  port = 80
+  port     = 80
   protocol = "HTTP"
-  vpc_id = "${aws_security_group.InstanceSecGroup.vpc_id}"
+  vpc_id   = "${aws_security_group.InstanceSecGroup.vpc_id}"
+
   health_check {
-    interval = 30
-    timeout = 5
-    healthy_threshold = 3
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
     unhealthy_threshold = 5
   }
+
   target_type = "instance"
+
   stickiness {
-    enabled = true
-    type = "lb_cookie"
+    enabled         = true
+    type            = "lb_cookie"
     cookie_duration = 180
   }
 }
 
 resource "aws_alb" "AppLoadBalancer" {
-  security_groups = "[${aws_security_group.InstanceSecGroup.id}]"
-  subnets = ["subnet-049eb8282c546ed44","subnet-0bbd9acc50009f861"]
+  security_groups = ["${aws_security_group.InstanceSecGroup.id}"]
+  subnets         = ["subnet-049eb8282c546ed44", "subnet-0bbd9acc50009f861"]
 }
 
 resource "aws_alb_listener" "AlbListener" {
   "default_action" {
     target_group_arn = "${aws_alb_target_group.AlbTargetGroup.arn}"
-    type = "forward"
+    type             = "forward"
   }
+
   load_balancer_arn = "${aws_alb.AppLoadBalancer.arn}"
-  port = 80
-  protocol = "HTTP"
+  port              = 80
+  protocol          = "HTTP"
 }
 
 resource "aws_instance" "web" {
-  instance_type = "t1.micro"
+  instance_type = "t2.micro"
+  key_name = "test"
+  subnet_id = "subnet-049eb8282c546ed44"
+  associate_public_ip_address = true
+  security_groups = ["${aws_security_group.InstanceSecGroup.id}"]
   tags {
     Name = "test_tf"
   }
-  ami = "ami-2a69aa47"
+  ami = "ami-cfe4b2b0"
+  user_data =  "#!/bin/bash -xe\n yum -y update\n yum -y install nginx\n service nginx restart\n"
+}
 
+resource "aws_alb_target_group_attachment" "AttachmentToInstance" {
+  port             = 80
+  target_group_arn = "${aws_alb_target_group.AlbTargetGroup.arn}"
+  target_id        = "${aws_instance.web.id}"
 }
