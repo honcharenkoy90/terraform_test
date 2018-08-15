e7774f86b7a1f7a57c5e1509421319607d61642d
yurii.honcharenko <honcharenkoy90@gmail.com>
2018-08-15 16:13:41 +0200

    Working connection to http with ALB

--- file.tf
+++ file.tf
@@ -6,11 +6,13 @@ resource "aws_security_group" "InstanceSecGroup" {
       from_port = 22
       protocol  = "tcp"
       to_port   = 22
+      cidr_blocks = ["0.0.0.0/0"]
     },
     {
       from_port = 80
       protocol  = "tcp"
       to_port   = 80
+      cidr_blocks = ["0.0.0.0/0"]
     },
   ]
 
@@ -19,11 +21,13 @@ resource "aws_security_group" "InstanceSecGroup" {
       from_port = 22
       protocol  = "tcp"
       to_port   = 22
+      cidr_blocks = ["0.0.0.0/0"]
     },
     {
       from_port = 80
       protocol  = "tcp"
       to_port   = 80
+      cidr_blocks = ["0.0.0.0/0"]
     },
   ]
 }
