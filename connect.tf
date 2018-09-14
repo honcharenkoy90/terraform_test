f5144f7b20fd73afd34755072a2df97e1addff19
yurii.honcharenko <honcharenkoy90@gmail.com>
2018-09-14 14:22:39 +0200

    Add backend to s3

--- connect.tf
+++ connect.tf
@@ -3,3 +3,13 @@ provider "aws" {
   shared_credentials_file = "/home/vagrant/.aws/creds"
   profile                 = "tf_user2"
 }
+
+terraform {
+  backend "s3" {
+    bucket                  = "trfm-bucke-test"
+    key                     = "test"
+    region                  = "us-east-1"
+    shared_credentials_file = "/home/vagrant/.aws/creds"
+    profile                 = "tf_user2"
+  }
+}
--- file.tf
+++ file.tf
@@ -3,30 +3,30 @@ resource "aws_security_group" "InstanceSecGroup" {
 
   ingress = [
     {
-      from_port = 22
-      protocol  = "tcp"
-      to_port   = 22
+      from_port   = 22
+      protocol    = "tcp"
+      to_port     = 22
       cidr_blocks = ["0.0.0.0/0"]
     },
     {
-      from_port = 80
-      protocol  = "tcp"
-      to_port   = 80
+      from_port   = 80
+      protocol    = "tcp"
+      to_port     = 80
       cidr_blocks = ["0.0.0.0/0"]
     },
   ]
 
   egress = [
     {
-      from_port = 22
-      protocol  = "tcp"
-      to_port   = 22
+      from_port   = 22
+      protocol    = "tcp"
+      to_port     = 22
       cidr_blocks = ["0.0.0.0/0"]
     },
     {
-      from_port = 80
-      protocol  = "tcp"
-      to_port   = 80
+      from_port   = 80
+      protocol    = "tcp"
+      to_port     = 80
       cidr_blocks = ["0.0.0.0/0"]
     },
   ]
